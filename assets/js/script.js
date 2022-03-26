98793da31ea21f7c51324fcf6ba1466701131b0e
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-03-26 18:40:24 -0400

    Update js logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,23 @@ function textAreaColor() {
   }
   
   textAreaColor();
+
+
+  $(".saveBtn").click(function () { 
+    // console.log(this);
+    var text = $(this).siblings(".schedule").val();
+    var time = $(this).parent().attr("id");
+    // console.log(text, time);
+    localStorage.setItem(time, text);
+    console.log(text);
+});
+
+$("#9 .schedule").val(localStorage.getItem("9"));
+$("#10 .schedule").val(localStorage.getItem("10"));
+$("#11 .schedule").val(localStorage.getItem("11"));
+$("12 .schedule").val(localStorage.getItem("12"));
+$("#13 .schedule").val(localStorage.getItem("13"));
+$("#14 .schedule").val(localStorage.getItem("14"));
+$("#15 .schedule").val(localStorage.getItem("15"));
+$("#16 .schedule").val(localStorage.getItem("16"));
+$("#17 .schedule").val(localStorage.getItem("17"));
